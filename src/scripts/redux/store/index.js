1bcc9b78b399cd48257f34a9070abfc638691166
QMATIC\joelan <joel.langstrom@qmatic.com>
2018-02-09 11:38:54 +0100

    Migration to jQuery 3.3.1 and removed unused files.

--- src/scripts/redux/store/index.js
+++
@@ -1,8 +0,0 @@
-window.$Qmatic.redux.store = (function (){ 
-        if(!!!window.Redux){
-            throw new Error("Redux Library Not Loaded!!!")
-        } else {
-            return Redux.createStore(window.$Qmatic.redux.reducers, {})
-        }
-})();
-
