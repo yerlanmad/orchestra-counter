c434d87dd6a8482e95e1bcde9f543c217db684b6
Lakmal Caldera <lakmal.developer@gmail.com>
2018-02-09 13:15:09 +0530

    dropdown expand fixed for ds selection

--- src/scripts/components/dropdown/deliveredServicesSelection/deliveredServicesSelection.js
+++ src/scripts/components/dropdown/deliveredServicesSelection/deliveredServicesSelection.js
@@ -3,9 +3,7 @@ $(function() {
         single_disable: false,
         allow_single_deselect: false,
         expand_dropdown_height: function (elem) {
-            var heightOfContentSection = elem.parent().parent().parent().parent().parent().parent().find(".qm-card__content-section").height() -
-                (elem.parent().parent().parent().parent().parent().parent().find(".chosen-search").height() +
-                elem.parent().parent().parent().parent().parent().parent().find(".custom-list-header").height() + 25);
+            var heightOfContentSection = elem.parent().parent().parent().parent().parent().parent().find(".qm-card__content-section").height() - 25;
 
             elem.parent().find(".chosen-results").css("max-height", heightOfContentSection);
         }
