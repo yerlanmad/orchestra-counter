a0c8c31ef8b075552d4c7466f770a988e5fdbb80
Lakmal Caldera <lakmal.developer@gmail.com>
2018-02-19 10:01:47 +0530

    Ui Bugs

--- src/scripts/components/dropdown/deliveredServicesSelection/deliveredServicesSelection.js
+++ src/scripts/components/dropdown/deliveredServicesSelection/deliveredServicesSelection.js
@@ -3,7 +3,7 @@ $(function() {
         single_disable: false,
         allow_single_deselect: false,
         expand_dropdown_height: function (elem) {
-            var heightOfContentSection = elem.parent().parent().parent().parent().parent().parent().find(".qm-card__content-section").height() - 25;
+            var heightOfContentSection = elem.parent().parent().parent().parent().parent().parent().find(".qm-card__content-section").height() - 65;
 
             elem.parent().find(".chosen-results").css("max-height", heightOfContentSection);
         }
